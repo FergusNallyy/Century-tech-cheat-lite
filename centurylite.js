3ea7df64c3a8346f14b08ff758fda5cfe0ac9bc9
Fergus Nally <12160@elizabethcollege.gg>
2026-03-18 12:09:10 +0000

    Update author name in centurylite.js

--- centurylite.js
+++ centurylite.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Lightweight auto-solver for Century Tech
-// @author       You
+// @author       Funguy
 // @match        https://*.century.tech/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=century.tech
 // @grant        GM_xmlhttpRequest
